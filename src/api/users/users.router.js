aacad91cd3c47c09bee747154b9cc970fb916e52
jaanmeld <jaanmeld@gmail.com>
2024-01-15 20:31:47 +0100

    route change /id:id => /:id

--- src/api/users/users.router.js
+++ src/api/users/users.router.js
@@ -3,6 +3,6 @@ import * as usersController from './users.controller.js';
 
 const router = Router();
 
-router.get('/id/:id', usersController.getById);
+router.get('/:id', usersController.getById);
 
 export default router;
