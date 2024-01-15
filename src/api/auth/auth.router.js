d0dfe24dc3daccf689ed21d4ef16418590bb5a51
vitaljik <meudza@inbox.lv>
2024-01-15 19:54:58 +0100

    removing unnecessary files

--- src/api/auth/auth.router.js
+++
@@ -1,9 +0,0 @@
-import Router from 'express';
-import * as authController from './auth.controller.js';
-
-const router = Router();
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-export default router;
