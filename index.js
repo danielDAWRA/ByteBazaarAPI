9e9ecf363ff7090298875a118ad0fc0a25ac51ca
Andrea Mir Olba <andreamirolba@MacBook-Pro-de-Drea.local>
2024-01-17 20:22:18 +0100

    Deleting innecesary files

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import './dB.js';
 import cors from 'cors';
 
 import apiRouter from './src/api/router.js';
-import isLogged from './src/middlewares/isLogged.js';
+// import isLogged from './src/middlewares/isLogged.js';
 
 const server = express();
 const { PORT } = process.env;
 
 server.use(express.json());
 server.use(cors({ origin: true }));
-server.use(isLogged);
+// server.use(isLogged);
 server.use(apiRouter);
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
--- src/api/router.js
+++ src/api/router.js
@@ -1,13 +1,13 @@
 import Router from 'express';
 
-import authRouter from './auth/auth.router.js';
+// import authRouter from './auth/auth.router.js';
 import gameTitlesRouter from './gameTitles/gameTitles.router.js';
 import platformsRouter from './platforms/platforms.router.js';
 import productsRouter from './products/products.router.js';
 
 const router = Router();
 
-router.use('/auth', authRouter);
+// router.use('/auth', authRouter);
 router.use('/gameTitles', gameTitlesRouter);
 router.use('/platforms', platformsRouter);
 router.use('/products', productsRouter);
