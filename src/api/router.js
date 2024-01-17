0e3c3b231623f97acb6b7f7b64f6a92294edad3e
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-01-17 19:20:44 +0100

    Deleted imports pointing to non-existent files

--- src/api/router.js
+++ src/api/router.js
@@ -1,7 +1,6 @@
 import Router from 'express';
 import genresRouter from './genres/genres.router.js';
 
-// import authRouter from './auth/auth.router.js';
 import gameTitlesRouter from './gameTitles/gameTitles.router.js';
 import platformsRouter from './platforms/platforms.router.js';
 import productsRouter from './products/products.router.js';
@@ -9,7 +8,6 @@ import productsRouter from './products/products.router.js';
 const router = Router();
 
 router.use('/genres', genresRouter);
-// router.use('/auth', authRouter);
 router.use('/gameTitles', gameTitlesRouter);
 
 router.use('/platforms', platformsRouter);
