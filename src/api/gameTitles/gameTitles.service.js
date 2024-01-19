5ec9405034d0c7823eeda2974ef4f37ee288904d
Andrea Mir Olba <andreamirolba@MacBook-Pro-de-Drea.local>
2024-01-19 18:46:33 +0100

    service changed

--- src/api/gameTitles/gameTitles.service.js
+++ src/api/gameTitles/gameTitles.service.js
@@ -6,24 +6,22 @@ async function getById({ id }) {
   return gameTitles;
 }
 
-// async function getByProductId({ productId }) {
-//   const product = await productsService.getById({ _id: productId });
-//   const gameTitleId = product.gameTitle_id;
-//   const gameTitles = await gameTitlesRepository.getByProductId({ gameTitleId });
-//   return gameTitles;
-// }
-
 async function getByProductId(productId) {
   try {
     const product = await productsService.getById({ _id: productId });
     console.log('product gameTitle.service:', product);
     console.log('product:', product);
 
+    let gameTitleId;
+
     if (product && product.gameTitle_id) {
-      const gameTitleId = product.gameTitle_id;
+      gameTitleId = product.gameTitle_id;
     } else {
       console.error('Product or gameTitle_id is null.');
+      return null;
     }
+
+    return gameTitleId;
   } catch (error) {
     console.error('Error:', error);
     throw error;
