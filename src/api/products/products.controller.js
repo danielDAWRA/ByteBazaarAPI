80e81179a60c953ec003c9c5950389758ef32017
nuinn <dilley.marc@gmail.com>
2024-01-22 19:47:57 +0100

    deleted a redundant line of code in controller

--- src/api/products/products.controller.js
+++ src/api/products/products.controller.js
@@ -1,8 +1,7 @@
 import * as productsService from './products.service.js';
 
 async function getAll(req, res) {
-  const { query } = req;
-  const { skip, limit } = query;
+  const { skip, limit } = req.query;
   const products = await productsService.getAll({ skip, limit });
   res.json({ products });
 }
