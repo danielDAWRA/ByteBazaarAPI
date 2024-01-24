08f6aac165b90aa2fde43e90f268927aff5f9cfd
Andrea Mir Olba <andreamirolba@MacBook-Pro-de-Drea.local>
2024-01-24 20:34:30 +0100

    wip

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import './dB.js';
 import cors from 'cors';
-
 import apiRouter from './src/api/router.js';
+import isAdmin from './src/middlewares/isAdmin.js';
 
 const server = express();
 const { PORT } = process.env;
@@ -10,6 +10,7 @@ const { PORT } = process.env;
 server.use(express.json());
 server.use(cors({ origin: true }));
 server.use(apiRouter);
+server.use(isAdmin);
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server started on port ${PORT}`);
---
+++ src/middlewares/isAdmin.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line consistent-return
+const isAdmin = async (req, res, next) => {
+  const admin = [true];
+  if (!admin.includes(req.user.isAdmin)) {
+    return res.status(403).send({
+      message: 'You do not have permission',
+    });
+  }
+  next();
+};
+export default isAdmin;
