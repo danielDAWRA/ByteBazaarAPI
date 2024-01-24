82ff2778f55937c3d616ffe0b2bc1393bf12ee13
nuinn <dilley.marc@gmail.com>
2024-01-24 19:33:33 +0100

    added isLogged middleware

---
+++ src/middlewares/isLogged.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+import * as userService from '../api/users/users.service.js';
+
+function unauthorized(res) {
+  res.status(401);
+  res.json({ msg: 'Unauthorised' });
+}
+
+function isLogged(req, res, next) {
+  const publicRoutes = [
+    '/auth/login',
+    '/auth/register',
+    '/auth/validate',
+  ];
+  const isPublicRoute = publicRoutes.some((publicRoute) => req.url.startsWith(publicRoute));
+  if (isPublicRoute) {
+    next();
+    return;
+  }
+  const token = req.headers.authorization;
+  if (!token) {
+    unauthorized(res);
+    return;
+  }
+  const { TOKEN_SECRET_WORD } = process.env;
+  jwt.verify(token, TOKEN_SECRET_WORD, async (error, payload) => {
+    if (error) {
+      console.error('jwt error');
+      unauthorized(res);
+      return;
+    }
+    const user = await userService.getById({ id: payload.userId });
+    req.user = user;
+    next();
+  });
+}
+
+export default isLogged;
