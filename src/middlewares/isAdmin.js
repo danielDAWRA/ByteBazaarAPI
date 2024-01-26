267cacaeafe888e556c561cbaeb420c8f9618b94
Andrea Mir Olba <andreamirolba@MacBook-Pro-de-Drea.local>
2024-01-26 20:14:58 +0100

    changed const for async function and admin as a boolean

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,11 +1,12 @@
 // eslint-disable-next-line consistent-return
-const isAdmin = async (req, res, next) => {
-  const admin = [true];
-  if (!admin.includes(req.user.isAdmin)) {
+async function isAdmin(req, res, next) {
+  const admin = true;
+  if (req.user.isAdmin !== admin) {
     return res.status(403).send({
       message: 'You do not have permission.',
     });
   }
   next();
-};
+}
+
 export default isAdmin;
