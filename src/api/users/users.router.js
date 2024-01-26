8989e170786c7477c270faa26060ac91d4610818
jaanmeld <meudza@inbox.lv>
2024-01-26 20:41:10 +0100

    Delete src/api/users/users.router.js

--- src/api/users/users.router.js
+++
@@ -1,9 +0,0 @@
-import Router from 'express';
-import * as usersController from './users.controller.js';
-
-const router = Router();
-
-router.get('/:id', usersController.getById);
-router.get('/byEmail/:email', usersController.getByEmail);
-
-export default router;
