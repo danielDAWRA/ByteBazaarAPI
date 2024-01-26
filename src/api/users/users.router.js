530128486a8e55cefcd82875738c7920161b3764
nuinn <dilley.marc@gmail.com>
2024-01-26 20:16:04 +0100

    removed commented line as requested

--- src/api/users/users.router.js
+++ src/api/users/users.router.js
@@ -4,7 +4,6 @@ import * as usersController from './users.controller.js';
 const router = Router();
 
 router.get('/id/:id', usersController.getById);
-// updated getById path as the path "/profile" was being interpreted as a params of getById!!
 router.get('/profile', usersController.getProfile);
 
 export default router;
