c7b2f4545adba95a4f13490492c8312318c29349
Andrea Mir Olba <andreamirolba@MacBook-Pro-de-Drea.local>
2024-01-26 19:34:37 +0100

    middleware removed from route

--- src/api/genres/genres.router.js
+++ src/api/genres/genres.router.js
@@ -1,10 +1,9 @@
 import Router from 'express';
 import * as genresController from './genres.controller.js';
-import isAdmin from '../../middlewares/isAdmin.js';
 
 const router = Router();
 
-router.get('/all', isAdmin, genresController.getAll);
+router.get('/all', genresController.getAll);
 router.get('/:id', genresController.getGenreById);
 
 export default router;
