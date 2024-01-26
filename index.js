382e68282900e3a67459e3adea47838ea278b2bf
Andrea Mir Olba <andreamirolba@MacBook-Pro-de-Drea.local>
2024-01-26 20:25:10 +0100

    removed changes from index.js and changed async functino

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import './dB.js';
 import cors from 'cors';
 import apiRouter from './src/api/router.js';
-import isAdmin from './src/middlewares/isAdmin.js';
 import isLogged from './src/middlewares/isLogged.js';
 
 const server = express();
@@ -12,7 +11,6 @@ server.use(express.json());
 server.use(cors({ origin: true }));
 server.use(isLogged);
 server.use(apiRouter);
-server.use(isAdmin);
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server started on port ${PORT}`);
--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line consistent-return
-async function isAdmin(req, res, next) {
+function isAdmin(req, res, next) {
   if (!req.user.isAdmin) {
     return res.status(403).send({
       message: 'You do not have permission.',
